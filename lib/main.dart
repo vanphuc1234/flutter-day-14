c51a53e595668bb0532ed052b9fa04ff84554200
VanPhuc <nphuc121166@gmail.com>
2022-07-06 17:27:05 +0700

    1

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
+import 'view/posts_view.dart';
+
+void main(List<String> args) {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: PostsView(),
+    );
+  }
+}
